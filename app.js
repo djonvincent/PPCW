81e7e8e1eaf1e9174f5e36ad3c85ded11f30cc2f
Dion <dionhs@hotmail.co.uk>
2018-11-08 11:32:30 +0000

    Add follow endpoint

--- app.js
+++ app.js
@@ -97,4 +97,27 @@ app.get('/api/login/', (req, res) => {
     res.send({'key': apiKey});
 });
 
+app.post('/api/follow/:username', auth, (req, res) => {
+    if (!User.get(req.params.username)) {
+        return res.status(400).send({'error': 'Specified user does not exist'});
+    }
+    if (req.user.follows.indexOf(req.params.username) !== -1) {
+        return res.status(400).send({'error': 'You already follow that user'});
+    }
+    req.user.follows.push(req.params.username);
+    res.send({'status': 'followed'});
+});
+
+app.delete('/api/follow/:username', auth, (req, res) => {
+    if (!User.get(req.params.username)) {
+        return res.status(400).send({'error': 'Specified user does not exist'});
+    }
+    let i = req.user.follows.indexOf(req.params.username);
+    if (i == -1) {
+        return res.status(400).send({'error': "You don't follow that user"});
+    }
+    req.user.follows.splice(i,1);
+    res.send({'status': 'unfollowed'});
+});
+
 app.listen(port);
