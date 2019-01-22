61de562a9f22ac9b32afc55e4079d828702b43e3
Dion <dionhs@hotmail.co.uk>
2019-01-22 16:37:48 +0000

    fix routes not changing if same route active

--- public/js/routes.js
+++ public/js/routes.js
@@ -18,7 +18,7 @@ document.querySelectorAll('div.page').forEach(el => {
 
 navigate(window.location.pathname);
 
-document.addEventListener('click', e  => {
+document.addEventListener('click', e => {
     let el = e.target;
     while (el && !(el.classList.contains('route') && el.tagName === 'A')) {
         el = el.parentElement;
@@ -44,7 +44,7 @@ function navigate(path) {
 		m = path.match(r.re)
 		return m;
 	});
-	if (!route || activeRoute === route) {
+	if (!route) {
 		return;
 	}
 	let params = {}
@@ -69,12 +69,12 @@ function loadPage (route, params) {
 function changeActiveNavItem(path) {
     let actives = document.querySelectorAll('.nav-item.active');
     let links = document.querySelectorAll(`a.route[href='${path}']`);
-    links.forEach(el => {
-        el.parentElement.classList.add('active');
-    });
-    if (links) {
+    if (actives) {
         actives.forEach(el => {
             el.classList.remove('active');
         });
     }
+    links.forEach(el => {
+        el.parentElement.classList.add('active');
+    });
 }
