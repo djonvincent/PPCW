f4496f13fa57c358acb8019e58285686486d0976
Dion <dionhs@hotmail.co.uk>
2019-01-31 21:38:36 +0000

    log address

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const app = require('./app.js');
 const server = require('http').createServer(app);
 const port = process.env.PORT || 3000;
 server.listen(port);
+console.log('Running at http://127.0.0.1:' + port);
